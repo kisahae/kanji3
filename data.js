60aad9bc3094cb9aad7e89cbbbd59ce31bbbd578
Kisahae <112599839+kisahae@users.noreply.github.com>
2024-06-14 23:19:13 +0700

    Update data.js

--- data.js
+++ data.js
@@ -46,5 +46,77 @@ export let kanji = [
           {
     hint: "Match",
     answer: "試合"
+  },
+             {
+    hint: "Citizen",
+    answer: "国民"
+  },
+                {
+    hint: "Population/Penduduk",
+    answer: "住民"
+  },
+                   {
+    hint: "Map",
+    answer: "地図"
+  },
+                     {
+    hint: "Examine/Meneliti",
+    answer: "研究する"
+  },
+                       {
+    hint: "Give Birth",
+    answer: "出産する"
+  },
+                         {
+    hint: "Measure/Mengukur",
+    answer: "測る"
+  },
+                            {
+    hint: "Memproduksi/Produce",
+    answer: "生産する"
+  },
+                               {
+    hint: "Cocok/Fit",
+    answer: "合う"
+  },
+                                {
+    hint: "Dead (Informal)",
+    answer: "死ぬ"
+  },
+                                   {
+    hint: "Dead (Formal)",
+    answer: "亡くなる"
+  },
+                                      {
+    hint: "Exam",
+    answer: "試験"
+  },
+                                         {
+    hint: "Bring Here",
+    answer: "持ってくる"
+  },
+                                           {
+    hint: "Bring There",
+    answer: "持って行く"
+  },
+                                              {
+    hint: "Bring",
+    answer: "持つ"
+  },
+                                                {
+    hint: "Finish/End",
+    answer: "終わる"
+  },
+                                                  {
+    hint: "Believe",
+    answer: "信じる"
+  },
+                                                     {
+    hint: "Maju/Berkembang/Forward",
+    answer: "進む"
+  },
+                                                        {
+    hint: "Close",
+    answer: "閉める"
   }
 ]
