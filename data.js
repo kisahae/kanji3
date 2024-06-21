ac4cb681a9f198425ab2c679cdd2f863de1017cc
Haenuki Sachi <sachihaenuki1@gmail.com>
2024-06-21 19:48:17 +0700

    Update Program

--- data.js
+++ data.js
@@ -1,187 +1,186 @@
 export let kanji = [
-   {
+  {
     hint: "Sidewalk",
     answer: "歩道"
   },
-    {
+  {
     hint: "Crosswalk",
     answer: "横断歩道"
   },
-       {
+  {
     hint: "Airline ticket",
     answer: "航空券"
   },
-          {
+  {
     hint: "Boarding Ticket",
     answer: "搭乗券"
   },
-             {
+  {
     hint: "Departure",
     answer: "出発"
   },
- {
+  {
     hint: "Amusement Park",
     answer: "遊園地"
   },
-    {
+  {
     hint: "Luggage",
     answer: "荷物"
   },
-{
+  {
     hint: "Make Up/Cosmetic",
     answer: "化粧品"
   },
-   {
+  {
     hint: "Plant/Vegetation",
     answer: "植物"
   },
-      {
+  {
     hint: "Parents",
     answer: "両親"
   },
-       {
+  {
     hint: "Western",
     answer: "洋"
   },
-          {
+  {
     hint: "Match",
     answer: "試合"
   },
-             {
+  {
     hint: "Citizen",
     answer: "国民"
   },
-                {
+  {
     hint: "Population/Penduduk",
     answer: "住民"
   },
-                   {
+  {
     hint: "Map",
     answer: "地図"
   },
-                     {
+  {
     hint: "Examine/Meneliti",
     answer: "研究する"
   },
-                       {
+  {
     hint: "Give Birth",
     answer: "出産する"
   },
-                         {
+  {
     hint: "Measure/Mengukur",
     answer: "測る"
   },
-                            {
+  {
     hint: "Memproduksi/Produce",
     answer: "生産する"
   },
-                               {
+  {
     hint: "Cocok/Fit",
     answer: "合う"
   },
-                                {
+  {
     hint: "Dead (Informal)",
     answer: "死ぬ"
   },
-                                   {
+  {
     hint: "Dead (Formal)",
     answer: "亡くなる"
   },
-                                      {
+  {
     hint: "Exam",
     answer: "試験"
   },
-                                         {
+  {
     hint: "Bring Here",
     answer: "持ってくる"
   },
-                                           {
+  {
     hint: "Bring There",
     answer: "持って行く"
   },
-                                              {
+  {
     hint: "Bring",
     answer: "持つ"
   },
-                                                {
+  {
     hint: "Finish/End",
     answer: "終わる"
   },
-                                                  {
+  {
     hint: "Believe",
     answer: "信じる"
   },
-                                                     {
+  {
     hint: "Maju/Berkembang/Forward",
     answer: "進む"
   },
-                                                        {
+  {
     hint: "Close",
     answer: "閉める"
   },
-   ,
- {
+  {
     hint: "Narrow",
     answer: "狭い"
-  } ,
-                                                        {
+  },
+  {
     hint: "Few",
     answer: "少ない"
   },
-   {
+  {
     hint: "Boring/usually written in hiragana",
     answer: "詰まらない"
   },
-     {
+  {
     hint: "Far",
     answer: "遠い"
   },
-   {
+  {
     hint: "Loud/usually written in hiragana",
     answer: "煩い"
   },
-      {
+  {
     hint: "Quiet (Adjective)",
     answer: "おとなしい"
   },
-        {
+  {
     hint: "Quiet (Environment)",
     answer: "静か"
   },
-           {
+  {
     hint: "Sweet",
     answer: "甘い"
   },
-           {
+  {
     hint: "Bitter",
     answer: "苦い"
   },
-              {
+  {
     hint: "Sour",
     answer: "酸っぱい"
   },
-                 {
+  {
     hint: "Kind",
     answer: "優しい"
   },
-                   {
+  {
     hint: "Painful",
     answer: "痛い"
   },
-                    {
+  {
     hint: "Dormitory",
     answer: "寮"
   },
-                       {
+  {
     hint: "Office",
     answer: "職場"
   },
-                         {
+  {
     hint: "Husband",
     answer: "主人"
   },
-                         {
+  {
     hint: "Wife",
     answer: "家内"
-  },
+  }
 ]
